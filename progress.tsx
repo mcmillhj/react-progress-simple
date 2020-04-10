cfe28a5761dce4ac3d589802c1be05f95c650c76
mcmillhj <mcmillhj@gmail.com>
2020-04-10 16:04:01 -0400

    rework how inline styles are applied, only send when needed

--- progress.tsx
+++ progress.tsx
@@ -2,25 +2,35 @@ import React, { CSSProperties } from "react";
 
 import "./progress.css";
 
-const defaultOptions = {
-  width: "100%",
-  height: "8px",
-  foregroundColor: "tomato",
-  backgroundColor: "lightgray",
-  borderRadius: "4px",
-  separatorWidth: "4px",
+type InlineStyles =
+  | "--rps-width"
+  | "--rps-height"
+  | "--rps-foreground-color"
+  | "--rps-background-color"
+  | "--rps-border-radius"
+  | "--rps-separator-width";
+
+const optionsMap: Record<keyof StyleOptions, InlineStyles> = {
+  width: "--rps-width",
+  height: "--rps-height",
+  foregroundColor: "--rps-foreground-color",
+  backgroundColor: "--rps-background-color",
+  borderRadius: "--rps-border-radius",
+  separatorWidth: "--rps-separator-width",
+};
+
+type StyleOptions = {
+  width?: CSSProperties["width"];
+  height?: CSSProperties["height"];
+  foregroundColor?: CSSProperties["color"];
+  backgroundColor?: CSSProperties["backgroundColor"];
+  borderRadius?: CSSProperties["borderRadius"];
+  separatorWidth?: CSSProperties["margin"];
 };
 
 type ProgressProps = {
   value: number;
-  options?: {
-    width?: CSSProperties["width"];
-    height?: CSSProperties["height"];
-    foregroundColor?: CSSProperties["color"];
-    backgroundColor?: CSSProperties["backgroundColor"];
-    borderRadius?: CSSProperties["borderRadius"];
-    separatorWidth?: CSSProperties["margin"];
-  };
+  options?: StyleOptions;
 } & (
   | { segmented?: false; max: number }
   | {
@@ -85,17 +95,17 @@ const Progress: React.SFC<ProgressProps> = ({
   options,
   ...restProps
 }) => {
-  const mergedOptions = { ...defaultOptions, ...options };
-
   // variables need to be inline to ensure that values are distinct across instances of the <Progress /> component
-  const inlineVariables = {
-    "--rps-width": mergedOptions.width,
-    "--rps-height": mergedOptions.height,
-    "--rps-foreground-color": mergedOptions.foregroundColor,
-    "--rps-background-color": mergedOptions.backgroundColor,
-    "--rps-border-radius": mergedOptions.borderRadius,
-    "--rps-segment-margin": mergedOptions.separatorWidth,
-  };
+  const inlineVariables = options
+    ? (Object.keys(options) as (keyof StyleOptions)[]).reduce(
+        (acc: Partial<Record<InlineStyles, any>>, current) => {
+          acc[optionsMap[current]] = options[current];
+
+          return acc;
+        },
+        {}
+      )
+    : {};
 
   if (!segmented) {
     // needed to style "completed" progress bars for varying sizes of 'max'
